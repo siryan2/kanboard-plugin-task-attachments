17f8ea98b2f731314ee8d98eb22c78a428fb3af6
Yannick Herzog <info@hit-services.net>
2018-12-19 17:07:03 +0100

    Add support to sort attachments by date desc.

--- Plugin.php
+++ Plugin.php
@@ -30,6 +30,9 @@ class Plugin extends Base
             'task_id' => $_GET['task_id'],
             'project_id' => $_GET['project_id'],
         ));
+
+        // Task details, sort attachments
+        $this->template->setTemplateOverride('task_file/files', 'TaskAttachments:task_file/files');
     }
 
     public function onStartup() {
